7fe0f0b84d72aae4f733427216dc078b81452533
Jinho Chun <jinhochun@Jinhoui-Macmini.local>
2022-01-31 07:53:47 +0900

    Change Mongo DB Env

--- index.js
+++ index.js
@@ -14,16 +14,13 @@ app.use(cors());
 app.use('/posts', postRoute);
 
 
-const CONNECTION_URL =
-  "mongodb://localhost:27017/memoriesDB?retryWrites=true&w=majority";
+const CONNECTION_URL = `mongodb://localhost:27017/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
 
 const PORT = process.env.PORT || 5001;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true,})
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
-
-
-
-
-
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
